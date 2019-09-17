47193922347b10d6df6ae231f82d87d6047fc152
siddheshmurudkar <32839650+siddheshmurudkar@users.noreply.github.com>
2019-09-17 14:47:55 +0530

    Update temperature_monitoring-AND-led_control.ino

--- temperature_monitoring-AND-led_control/temperature_monitoring-AND-led_control.ino
+++ temperature_monitoring-AND-led_control/temperature_monitoring-AND-led_control.ino
@@ -1,1 +1,185 @@
+/***************************************************
+  Adafruit ESP8266 Sensor Module
+
+  Must use ESP8266 Arduino from:
+    https://github.com/esp8266/Arduino
+  Works great with Adafruit's Huzzah ESP board:
+  ----> https://www.adafruit.com/product/2471
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+  Written by Tony DiCola for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+// Libraries
+#include <ESP8266WiFi.h>
+#include <Servo.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include "DHT.h"
+
+// DHT 11 sensor
+#define DHTPIN 14
+#define DHTTYPE DHT11
+
+
+// Lamp pin
+const int led_pin = 2;
+
+// WiFi parameters
+#define WLAN_SSID       "DT_LAB"
+#define WLAN_PASS       "fthu@050318"
+
+// Adafruit IO
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "siddhesh"
+#define AIO_KEY         "402abd741aae4a42b3847b22c71e6693"  // Obtained from account info on io.adafruit.com
+
+// DHT sensor
+DHT dht(DHTPIN, DHTTYPE, 15);
+
+WiFiClient client;
+ 
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+ 
+/****************************** Feeds ***************************************/
+ 
+// Setup a feed called 'lamp' for subscribing to changes.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Subscribe lamp = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/Led");
+// Setup feeds for temperature & humidity
+Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temperature");
+ 
+Adafruit_MQTT_Publish humidity = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/humidity");
+
+unsigned long currentMillis = 0;
+int interval = 20000;
+
+/*************************** Sketch Code ************************************/
+
+void setup() {
+
+  // Set lamp pin to output
+  pinMode(led_pin, OUTPUT);
+  // Init sensor
+  dht.begin();
+  
+  Serial.begin(115200);
+  Serial.println(F("Adafruit IO Example"));
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  delay(10);
+  Serial.print(F("Connecting to "));
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+  Serial.println();
+
+  Serial.println(F("WiFi connected"));
+  Serial.println(F("IP address: "));
+  Serial.println(WiFi.localIP());
+
+  // listen for events on the lamp feed
+  mqtt.subscribe(&Led);
+
+  // connect to adafruit io
+  connect();
+
+}
+
+void loop() {
+  
+  Adafruit_MQTT_Subscribe *subscription;
+  pirState = digitalRead(pirPin);
+  doorPinState = digitalRead(doorPin);
+  // ping adafruit io a few times to make sure we remain connected
+  if(! mqtt.ping(3)) {
+    // reconnect to adafruit io
+    if(! mqtt.connected())
+      connect();
+  }
+
+  // Grab the current state of the sensor
+  int humidity_data = (int)dht.readHumidity();
+  int temperature_data = (int)dht.readTemperature();
+
+  // By default, the temperature report is in Celsius, for Fahrenheit uncomment
+  //    following line.
+  // temperature_data = temperature_data*(9.0/5.0) + 32.0;
+ if(millis() - currentMillis > interval){
+  // Publish data
+  if (! temperature.publish(temperature_data))
+    Serial.println(F("Failed to publish temperature"));
+  else
+    Serial.println(F("Temperature published!"));
+
+  if (! humidity.publish(humidity_data))
+    Serial.println(F("Failed to publish humidity"));
+  else
+    Serial.println(F("Humidity published!"));
+  
+  currentMillis = millis();  
+ }   
+ 
+  // this is our 'wait for incoming subscription packets' busy subloop
+  while (subscription = mqtt.readSubscription(1000)) {
+
+    // we only care about the lamp events
+    if (subscription == &Led) {
+
+      // convert mqtt ascii payload to int
+      char *value = (char *)Led.lastread;
+      Serial.print(F("Received: "));
+      Serial.println(value);
+
+      // Apply message to lamp
+      String message = String(value);
+      message.trim();
+      if (message == "ON") {digitalWrite(led_pin, LOW);}
+      if (message == "OFF") {digitalWrite(led_pin, HIGH);}
+
+    }
+
+  }
+
+}
+
+// connect to adafruit io via MQTT
+void connect() {
+
+  Serial.print(F("Connecting to Adafruit IO... "));
+
+  int8_t ret;
+
+  while ((ret = mqtt.connect()) != 0) {
+
+    switch (ret) {
+      case 1: Serial.println(F("Wrong protocol")); break;
+      case 2: Serial.println(F("ID rejected")); break;
+      case 3: Serial.println(F("Server unavail")); break;
+      case 4: Serial.println(F("Bad user/pass")); break;
+      case 5: Serial.println(F("Not authed")); break;
+      case 6: Serial.println(F("Failed to subscribe")); break;
+      default: Serial.println(F("Connection failed")); break;
+    }
+
+    if(ret >= 0)
+      mqtt.disconnect();
+
+    Serial.println(F("Retrying connection..."));
+    delay(5000);
+
+  }
+
+  Serial.println(F("Adafruit IO Connected!"));
+
+}
 
